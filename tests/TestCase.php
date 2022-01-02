a15e727f4bdbb833b310af220c77c3d1989b8c5f
rocknrold <bsitharoldaaron@gmail.com>
2022-01-02 18:01:59 +0800

    admin aut feature test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createToken(int $roleId)
+    {
+        $user = User::where('role', $roleId)->inRandomOrder()->first();  
+        $token = $user->createToken($user->email. ' '. $user->fname, [$user->userRole()])->accessToken;
+        return $token;
+    }
+
 }
